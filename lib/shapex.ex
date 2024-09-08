cbd3bfefa342c4829c968629348d429d80cf6eac
Ilya Bayaliyev <ilya.bayaliyev@remote.com>
2024-09-08 15:06:26 +0500

    feat: add interpolated runtime values support

--- lib/shapex.ex
+++ lib/shapex.ex
@@ -14,6 +14,7 @@ defmodule Shapex do
     - map
     - record
     - string
+    - dict
   """
 
   @spec validate(Shapex.Type.t(), term()) :: {:ok, :valid} | {:error, term()}
@@ -21,91 +22,7 @@ defmodule Shapex do
     schema.__struct__.validate(schema, value)
   end
 
-  defmacro schema(schema) do
-    definition = gen_schema(schema)
-
-    quote do
-      unquote(Macro.escape(definition))
-    end
-  end
-
-  defp gen_schema(schema) do
-    case schema do
-      boolean when is_boolean(boolean) ->
-        Shapex.Types.boolean(boolean)
-
-      atom when is_atom(atom) ->
-        Shapex.Types.atom(eq: atom)
-
-      enum when is_list(enum) ->
-        gen_enum_schema(enum)
-
-      float when is_float(float) ->
-        Shapex.Types.float(eq: float)
-
-      int when is_integer(int) ->
-        Shapex.Types.integer(eq: int)
-
-      string when is_binary(string) ->
-        Shapex.Types.string(eq: string)
-
-      {:%{}, _, keyword} ->
-        gen_map_schema(keyword)
-
-      {:atom, _, args} ->
-        args
-        |> Enum.at(0, [])
-        |> Shapex.Types.atom()
-
-      {:boolean, _, [expected_value]} ->
-        Shapex.Types.boolean(expected_value)
-
-      {:dict, _, keyword} ->
-        gen_dict_schema(keyword)
-
-      {:float, _, args} ->
-        args
-        |> Enum.at(0, [])
-        |> Shapex.Types.float()
-
-      {:integer, _, args} ->
-        args
-        |> Enum.at(0, [])
-        |> Shapex.Types.integer()
-
-      {:list, _, [list_item]} ->
-        Shapex.Types.list(list_item)
-
-      {:string, _, args} ->
-        args
-        |> Enum.at(0, [])
-        |> Shapex.Types.string()
-
-      {v, _, _} ->
-        raise "Unknown schema type: #{inspect(v)}"
-    end
-  end
-
-  defp gen_map_schema(keyword) do
-    keyword
-    |> Enum.map(fn {key, value} -> {key, gen_schema(value)} end)
-    |> Enum.into(%{})
-    |> Shapex.Types.map()
-  end
-
-  defp gen_enum_schema(values) do
-    case Enum.find(values, fn value -> is_list(value) end) do
-      nil -> Shapex.Types.enum(values |> Enum.map(&gen_schema/1))
-      _ -> raise "There should be no nested enumerations"
-    end
-  end
-
-  defp gen_dict_schema(keyword) do
-    [{key, _, key_args}, {value, _, value_args}] = keyword
-
-    Shapex.Types.dict(
-      apply(Shapex.Types, key, key_args),
-      apply(Shapex.Types, value, value_args)
-    )
+  defmacro schema(expression) do
+    Services.Builder.build(expression)
   end
 end
--- test/schema_test.exs
+++ test/schema_test.exs
@@ -1,43 +1,45 @@
 defmodule Shapex.SchemaTest do
   use ExUnit.Case, async: true
 
+  alias Shapex.Types, as: S
+
   require Shapex
 
   describe "schema/1" do
     test "should return valid schema for integer" do
       schema = Shapex.schema(1)
 
-      assert Shapex.Types.integer(eq: 1) == schema
+      assert S.integer(eq: 1) == schema
     end
 
     test "should return valid schema for float" do
       schema = Shapex.schema(1.0)
 
-      assert Shapex.Types.float(eq: 1.0) == schema
+      assert S.float(eq: 1.0) == schema
     end
 
     test "should return valid schema for atom" do
       schema = Shapex.schema(:atom)
 
-      assert Shapex.Types.atom(eq: :atom) == schema
+      assert S.atom(eq: :atom) == schema
     end
 
     test "should return valid schema for string" do
       schema = Shapex.schema("string")
 
-      assert Shapex.Types.string(eq: "string") == schema
+      assert S.string(eq: "string") == schema
     end
 
     test "shoould generate schema for enums" do
       schema = Shapex.schema([1, 1.0, "string", :atom, true, %{key: "value"}])
 
-      assert Shapex.Types.enum([
-               Shapex.Types.integer(eq: 1),
-               Shapex.Types.float(eq: 1.0),
-               Shapex.Types.string(eq: "string"),
-               Shapex.Types.atom(eq: :atom),
-               Shapex.Types.boolean(true),
-               Shapex.Types.map(%{key: Shapex.Types.string(eq: "value")})
+      assert S.enum([
+               S.integer(eq: 1),
+               S.float(eq: 1.0),
+               S.string(eq: "string"),
+               S.atom(eq: :atom),
+               S.boolean(true),
+               S.map(%{key: S.string(eq: "value")})
              ]) == schema
     end
 
@@ -51,12 +53,12 @@ defmodule Shapex.SchemaTest do
           bool: true
         })
 
-      assert Shapex.Types.map(%{
-               int: Shapex.Types.integer(eq: 1),
-               float: Shapex.Types.float(eq: 1.0),
-               atom: Shapex.Types.atom(eq: :atom),
-               string: Shapex.Types.string(eq: "string"),
-               bool: Shapex.Types.boolean(true)
+      assert S.map(%{
+               int: S.integer(eq: 1),
+               float: S.float(eq: 1.0),
+               atom: S.atom(eq: :atom),
+               string: S.string(eq: "string"),
+               bool: S.boolean(true)
              }) == schema
     end
 
@@ -68,20 +70,64 @@ defmodule Shapex.SchemaTest do
           }
         })
 
-      assert Shapex.Types.map(%{
+      assert S.map(%{
                map:
-                 Shapex.Types.map(%{
-                   int: Shapex.Types.integer(eq: 1)
+                 S.map(%{
+                   int: S.integer(eq: 1)
                  })
              }) == schema
     end
 
-    test "d" do
+    test "should generate dict schema" do
       schema = Shapex.schema(dict(string(), integer(gte: 10)))
 
-      schema2 = Shapex.Types.dict(Shapex.Types.string(), Shapex.Types.integer(gte: 10))
+      assert schema == S.dict(S.string(), S.integer(gte: 10))
+    end
+  end
 
-      assert schema == schema2
+  describe "advanced cases" do
+    test "should generate maps with interpolated runtime values" do
+      user =
+        S.map(%{
+          name: S.string(min_length: 3),
+          age: S.integer(gt: 0)
+        })
+
+      enum_int = S.integer(eq: 1)
+      enum_float = S.float(eq: 1.0)
+
+      int_value = 10
+      float_value = 10
+      string_length = 8
+      not_atom = :not_atom
+      expected_bool = false
+      key = S.string(eq: "hello")
+      value = S.integer(eq: 10)
+
+      expected_schema =
+        S.map(%{
+          atom: S.atom(neq: not_atom),
+          bool: S.boolean(expected_bool),
+          dict: S.dict(key, value),
+          enum: S.enum([enum_int, enum_float]),
+          float: S.float(gt: float_value),
+          int: S.integer(gte: int_value),
+          list: S.list(S.integer(gte: 10)),
+          map: user,
+          string: S.string(min_length: string_length)
+        })
+
+      assert Shapex.schema(%{
+               atom: atom(neq: ^not_atom),
+               bool: boolean(^expected_bool),
+               dict: dict(^key, ^value),
+               enum: [^enum_int, ^enum_float],
+               float: float(gt: ^float_value),
+               int: integer(gte: ^int_value),
+               list: list(integer(gte: 10)),
+               map: ^user,
+               string: string(min_length: ^string_length)
+             }) == expected_schema
     end
   end
 end
